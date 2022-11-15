c34968ac0fc22c361753c2392f61779aad2e9b3c
roshan.bhattarai <roshan.bhattarai@diagonal.software>
2022-11-15 20:20:38 +0545

    added disabled props for button

--- frontend/packages/ui/button/index.tsx
+++ frontend/packages/ui/button/index.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 interface ButtonProps {
   text: string;
   onClick: () => void;
+  disabled?: boolean;
 }
 
 export const Button = (props: ButtonProps) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+  return (
+    <button onClick={props.onClick} disabled={props.disabled}>
+      {props.text}
+    </button>
+  );
 };
 
 export default Button;
